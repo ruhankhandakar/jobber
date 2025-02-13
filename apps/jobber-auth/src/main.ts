3f9105dba0d635444e278923412e6fe081b9afa3
Ruhan Khandakar <khandakarruhan@gmail.com>
2025-02-13 09:22:51 +0530

    remove eslint-disable comments from global teardown and test setup files

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = app.get(ConfigService).getOrThrow('PORT') || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
